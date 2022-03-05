433eaea78338d6beb2a06cc5790bd4ad3d53697d
mellobacon <liz6757@outlook.com>
2022-03-05 06:24:16 -0600

    Added command for removing directory

---
+++ MelloShell/Commands/RemoveDirectory.cs
@@ -0,0 +1,38 @@
+﻿namespace MelloShell.Commands;
+
+[CommandAttribute("rmdir", Aliases = new []{"rm"})]
+public class RemoveDirectory : ICommand
+{
+    public void Run(string[]? input)
+    {
+        switch (input!.Length)
+        {
+            case 1:
+                try
+                {
+                    Directory.Delete($@"{Directory.GetCurrentDirectory()}\{input[0]}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+                return;
+            case 2:
+                try
+                {
+                    if (Directory.Exists(input[0]))
+                    {
+                        Directory.Delete($@"{input[0]}\{input[1]}");
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+                return;
+            default:
+                Console.WriteLine($"Error: No arguments given");
+                return;
+        }
+    }
+}
