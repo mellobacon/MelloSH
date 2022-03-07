0f864660e5cfea737b96ff97ef4271b205ad8913
mellobacon <mellodev@outlook.com>
2022-03-07 15:24:45 -0600

    Added command for copying files....this doesnt work yet but yknow...it exists

---
+++ MelloShell/Commands/CopyFile.cs
@@ -0,0 +1,35 @@
+﻿namespace MelloShell.Commands;
+
+[CommandAttribute("cp")]
+public class CopyFile : ICommand
+{
+    public void Run(string[] input)
+    {
+        string filename;
+        string path;
+        switch (input.Length)
+        {
+            case 1:
+                path = Directory.GetCurrentDirectory();
+                filename = input[0];
+                break;
+            case 2:
+                path = input[1];
+                filename = input[0];
+                break;
+            default:
+                Console.WriteLine("Error: args not valid or something...idk");
+                return;
+        }
+
+        if (Directory.Exists(path))
+        {
+            bool overwrite = File.Exists($@"{path}\{filename}");
+            File.Move(filename, path, overwrite);
+        }
+        else
+        {
+            Console.WriteLine($"{path} does not exist");
+        }
+    }
+}
