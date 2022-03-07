e0b7dafd8985a7ec045382cb4c3000cda876d121
mellobacon <mellodev@outlook.com>
2022-03-07 15:10:50 -0600

    Added command for deleting files

---
+++ MelloShell/Commands/RemoveFile.cs
@@ -0,0 +1,33 @@
+﻿namespace MelloShell.Commands;
+
+[CommandAttribute("rm")]
+public class RemoveFile : ICommand
+{
+    public void Run(string[] input)
+    {
+        string filename;
+        var path = "";
+        switch (input.Length)
+        {
+            case 1:
+                filename = input[0];
+                break;
+            case 2:
+                filename = input[0];
+                path = input[1];
+                break;
+            default:
+                Console.WriteLine("Error: args not valid or something...idk");
+                return;
+        }
+
+        if (Directory.Exists(path))
+        {
+            File.Delete($@"{path}\{filename}");
+        }
+        else
+        {
+            Console.WriteLine($"{path} does not exist");
+        }
+    }
+}
