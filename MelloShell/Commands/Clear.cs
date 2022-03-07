a1285b0f6b9dc1cec9e9317846377a939d8fd26d
mellobacon <mellodev@outlook.com>
2022-03-07 15:09:51 -0600

    Added clear command

---
+++ MelloShell/Commands/Clear.cs
@@ -0,0 +1,10 @@
+﻿namespace MelloShell.Commands;
+
+[CommandAttribute("clear", Aliases = new []{"cls", "clr"})]
+public class Clear : ICommand
+{
+    public void Run(string[] input)
+    {
+        Console.Clear();
+    }
+}
