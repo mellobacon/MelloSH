02d788356cfc43679b59b2ca65baf139da25390d
mellobacon <mellodev@outlook.com>
2022-03-07 15:10:32 -0600

    Added command for creating files

---
+++ MelloShell/Commands/CreateFile.cs
@@ -0,0 +1,33 @@
+﻿namespace MelloShell.Commands;
+
+[CommandAttribute("touch", Aliases = new []{"createfile"})]
+public class CreateFile : ICommand
+{
+    public void Run(string[] input)
+    {
+        string filename;
+        var path = "";
+        switch (input.Length)
+        {
+            case 1:
+                filename = input[0];
+                break;
+            case 2:
+                filename = input[0];
+                path = input[1];
+                break;
+            default:
+                Console.WriteLine("Error: args not valid or something...idk");
+                return;
+        }
+
+        if (Directory.Exists(path))
+        {
+            File.Create($@"{path}\{filename}");
+        }
+        else
+        {
+            Console.WriteLine($"{path} does not exist");
+        }
+    }
+}
