d40a43b2484d951aade846a097ba61fe722dca01
mellobacon <mellodev@outlook.com>
2022-03-09 19:28:35 -0600

    Added command for renaming files

---
+++ MelloShell/Commands/RenameFile.cs
@@ -0,0 +1,31 @@
+﻿namespace MelloShell.Commands;
+
+[CommandAttribute("rename")]
+public class RenameFile : ICommand
+{
+    public void Run(string[] input)
+    {
+        switch (input.Length)
+        {
+            case 2:
+                var originalname = input[0];
+                var newname = input[1];
+                if (originalname.Equals(newname))
+                {
+                    Console.WriteLine($"Error: {originalname} and {newname} are the same file or name or whatever");
+                }
+                else if (File.Exists(originalname))
+                {
+                    File.Move(originalname, newname, true);
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {originalname} doesnt exist");
+                }
+                return;
+            default:
+                Console.WriteLine("Error: args not valid or something...idk");
+                return;
+        }
+    }
+}
