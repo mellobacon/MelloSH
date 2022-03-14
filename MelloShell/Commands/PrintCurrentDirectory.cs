8f678710d2eb6b7c6b9fb6ec4284d0586abfdf79
mellobacon <mellodev@outlook.com>
2022-03-14 18:52:25 -0500

    pwd

---
+++ MelloShell/Commands/PrintCurrentDirectory.cs
@@ -0,0 +1,29 @@
+﻿namespace MelloShell.Commands;
+
+/// <summary>
+/// Prints the current directory <br/>
+/// Usage: <c>pwd [options]</c> <br/>
+/// Options: <br/>
+/// -h, --h, --help: Prints this help text <br/>
+/// Example: <br/>
+/// Prints the current directory...self explanitory
+/// <code>
+///     pwd
+/// </code>
+/// </summary>
+[CommandAttribute("pwd")]
+public class PrintCurrentDirectory : ICommand
+{
+    public void Run(string[] input)
+    {
+        switch (input.Length)
+        {
+            case 0:
+                Console.Out.WriteLine($"Current Directory: {Directory.GetCurrentDirectory()}");
+                return;
+            default:
+                Console.WriteLine("Error: Type --help...i cant be bothered to explain how one messes up this command");
+                return;
+        }
+    }
+}
