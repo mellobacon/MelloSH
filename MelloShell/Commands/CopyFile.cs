a2e00e1e8beac0cb4d4126bb5b51a9db0e23e41c
mellobacon <liz6757@outlook.com>
2022-03-16 14:44:57 -0500

    Attempt n at fixing cp for directories

--- MelloShell/Commands/CopyFile.cs
+++ MelloShell/Commands/CopyFile.cs
@@ -1,7 +1,7 @@
 ﻿namespace MelloShell.Commands;
 
 /// <summary>
-/// Copies a file or multiple files to a directory. <br/>
+/// Copies a file/directory or multiple files/directories to a destination directory. If file exits it will overwrite. <br/>
 /// <remarks>Currently doesnt support copying directories.</remarks>
 /// Usage: <c>cp [options] [source] [dest]</c> <br/>
 /// Options: <br/>
@@ -40,20 +40,55 @@ public class CopyFile : ICommand
 
     private static void CopyFiles(string source, string dest)
     {
-        // Files cant be a directory
-        if (source.Contains('\\') || source.Contains('/'))
+        if (Directory.Exists(source))
         {
-            Console.WriteLine($"Error: {source} cannot be a directory. Skipping.");
+            string path = $@"{dest}\{source.Split(@"\")[^1]}";
+            Directory.CreateDirectory(path);
+            if (Directory.GetFiles(source).Length > 0)
+            {
+                var files = Directory.GetFiles(source);
+                foreach (string file in files)
+                {
+                    var filename = file.Split(@"\")[^1];
+                    CopyFiles($@"{source}\{filename}", $@"{path}\{filename}");
+                }
+            }
+
+            if (Directory.GetDirectories(source).Length > 0)
+            {
+                var dirs = Directory.GetDirectories(source);
+                foreach (string dir in dirs)
+                {
+                    CopyFiles(dir, path);
+                }
+            }
             return;
         }
-        
-        // TODO: fix "being used by another process" error. probably just need to close the stream
-        if (Directory.Exists(dest))
+
+        // TODO: fix parsing filepaths because doing something like 'path/' works but 'path/to/whatever' does not because my code is stupid
+        string sourcename = source;
+        string destname = dest;
+        if (!Directory.Exists(dest))
         {
-            bool overwrite = File.Exists($@"{dest}\{source}");
+            if (dest.Contains('\\'))
+            {
+                destname = dest.Remove(dest.LastIndexOf('\\'));
+            }
+            else if (dest.Contains('/'))
+            {
+                destname = dest.Remove(dest.LastIndexOf('/'));
+            }
+        }
+        if (Directory.Exists(destname))
+        {
+            if (source.Contains('\\'))
+            {
+                sourcename = source.Split(@"\")[^1];
+            }
+            bool overwrite = File.Exists($@"{destname}\{sourcename}");
             try
             {
-                File.Copy(source, $@"{dest}\{source}", overwrite);
+                File.Copy(source, $@"{destname}\{sourcename}", overwrite);
             }
             catch (Exception e)
             {
